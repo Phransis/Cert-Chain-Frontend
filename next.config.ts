ef4d3c4122f954697aa25553b5e5cce747a262f0
Handla <duofrancis11@gmail.com>
2026-06-07 16:56:47 +0000

    Add axios dependency and update tsconfig for type definitions; configure turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['172.20.10.2'],
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
